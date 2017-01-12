784e39c219e6328d64b3276eb60b7ef21c511728
William Dussault <dalloriam@gmail.com>
2017-01-12 11:23:16 -0500

    Start work on tests

---
+++ src/layouts/Layout/spec/Layout.spec.tsx
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import * as React from 'react';
+import { shallow } from 'enzyme';
+
+import { Layout } from '..';
+
+describe('Main Layout component', () => {
+    it('should exist', () => {
+        
+    });
+});
