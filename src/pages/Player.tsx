da0d160cab72e8bcaf63dfa13338d06977483d4e
kaizkiller <kaizkiller@bitbucket.org>
2017-02-18 22:09:12 -0500

    Ajout du code necessaire a l'affichage du nom du joueur. Des modifs doivent etre apportees ailleurs

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -6,6 +6,7 @@ import {HeatMap} from "../components/HeatMap/Map"
 import StatsTable from "../components/PlayerStats/StatsTable";
 import StatsGraphs from "../components/PlayerStats/StatsGraphs";
 import GenericMetricsView from "../components/genericMetrics/GenericMetricsView";
+import StatsTableStore from "../components/PlayerStats/store";
 
 import { Panel } from "react-bootstrap";
 
@@ -18,12 +19,30 @@ export interface ILayoutProps {
     }
 }
 
-export interface ILayoutState {}
+export interface ILayoutState {
+    playerName?: string
+}
 
 export default class Player extends React.Component<ILayoutProps, ILayoutState> {
     constructor() {
         super();
+        // this.getPlayerName = this.getPlayerName.bind(this);
     }
+
+    /* componentWillMount() {
+        StatsTableStore.on("dataChange", this.getPlayerName);
+    }
+
+    componentWillUnmount() {
+        StatsTableStore.removeListener("dataChange", this.getPlayerName);
+    }
+
+    // Va récupérer les joueurs du store.
+     getPlayerName() {
+        this.setState({
+            playerName: StatsTableStore.getPlayerName()
+        });
+    } */
 
     render() {
         let arrowTitle = <h3>Tracé des actions</h3>;
@@ -35,8 +54,8 @@ export default class Player extends React.Component<ILayoutProps, ILayoutState> 
             <div>
                 <Panel header={arrowTitle} className="data-panel"><ArrowMap/></Panel>
                 <Panel header={heatmapTitle} className="data-panel"><HeatMap/></Panel>
-                <Panel header={statsTitle} className="data-panel"><StatsTable playerID={this.props.params.playerID} teamID={this.props.params.teamID}/></Panel>
                 <Panel header={graphTitle} className="data-panel"><StatsGraphs playerID={this.props.params.playerID} teamID={this.props.params.teamID}/></Panel>
+                <Panel header={statsTitle} className="data-panel"><StatsTable playerID={this.props.params.playerID} teamID={this.props.params.teamID}/></Panel>
             </div>
         );
     }
