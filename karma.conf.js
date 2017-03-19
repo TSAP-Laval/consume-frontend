19ee2a82046278f2b2d694b8e64f8e8b24acc577
kaizkiller <kaizkiller@bitbucket.org>
2017-03-19 07:54:08 -0400

    Changed Karma unit testing reporter to spec.

--- karma.conf.js
+++ karma.conf.js
@@ -1,57 +1,55 @@
 var webpackConfig = require('./webpack.config');
 
-module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['mocha', 'chai', 'sinon'],
-    files: [
-      './test/test_index.js'
-    ],
-    exclude: [
-    ],
-    preprocessors: {
-      './test/test_index.js': ['webpack', 'sourcemap']
-    },
-    webpack: {
-      devtool: 'inline-source-map',
-      module: {
-          loaders: [
-              {
-                  test: /\.(ts|tsx)$/,
-                  exclude: /node_modules/,
-                  loader: 'ts-loader'
+module.exports = function(config) {
+    config.set({
+        basePath: '',
+        frameworks: ['mocha', 'chai', 'sinon'],
+        files: [
+            './test/test_index.js'
+        ],
+        exclude: [],
+        preprocessors: {
+            './test/test_index.js': ['webpack', 'sourcemap']
+        },
+        webpack: {
+            devtool: 'inline-source-map',
+            module: {
+                loaders: [{
+                        test: /\.(ts|tsx)$/,
+                        exclude: /node_modules/,
+                        loader: 'ts-loader'
+                    },
+                    //Configuration required by enzyme
+                    {
+                        test: /\.json$/,
+                        loader: 'json'
+                    }
+                ]
+            },
+            resolve: {
+                //Added .json extension required by cheerio (enzyme dependency)
+                extensions: ['', '.js', '.ts', '.tsx', '.json']
             },
             //Configuration required by enzyme
-            {
-                test: /\.json$/,
-                loader: 'json'
+            externals: {
+                'react/addons': true,
+                'react/lib/ExecutionEnvironment': true,
+                'react/lib/ReactContext': 'window',
             }
-          ]
-      },
-      resolve: {
-          //Added .json extension required by cheerio (enzyme dependency)
-          extensions: ['', '.js', '.ts', '.tsx', '.json']
-      },
-      //Configuration required by enzyme
-      externals: {
-        'react/addons': true,
-        'react/lib/ExecutionEnvironment': true,
-        'react/lib/ReactContext': 'window',
-      }
-    },
-    webpackMiddleware: {
-        // webpack-dev-middleware configuration
-        // i. e.
-        noInfo: true
-    },
+        },
+        webpackMiddleware: {
+            // webpack-dev-middleware configuration
+            // i. e.
+            noInfo: true
+        },
 
-    reporters: ['progress'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: true,
-    concurrency: Infinity
-  })
+        reporters: ['spec'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['Chrome'],
+        singleRun: true,
+        concurrency: Infinity
+    })
 }
