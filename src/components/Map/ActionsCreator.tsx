d2e1692795774b79ca01b1aed82b1f4c6fba5cff
Gabriel Méthot <goub.methot@gmail.com>
2017-03-19 22:41:11 -0400

    Mini fixs avant dodo

--- src/components/Map/ActionsCreator.tsx
+++ src/components/Map/ActionsCreator.tsx
@@ -10,16 +10,17 @@ export function getActions(match_id: number, player_id: number) {
     const fetch_actions = new Actions.FetchActions()
     dispatcher.dispatch(fetch_actions)
 
-    var url: string
-    url = serverUrl + "/stats/match/" + match_id + "/player/" + player_id
+    let url = serverUrl + "/stats/match/" + match_id + "/player/" + player_id
 
     axios.get(url).then((response) => {
         var data = response.data.actions
         var actions = data.map((action: any) => {
+            let action_type = new Models.ActionType(action.TypeAction.name, action.TypeAction.description)
+
             if(action.x2 != -1 && action.y2 != -1) {
-                return new Models.Action(action.TypeAction.name, action.is_valid, new Models.Coordinate(action.x1, action.y1), new Models.Coordinate(action.x2, action.y2))
+                return new Models.Action(action_type, new Models.Coordinate(action.x1, action.y1), new Models.Coordinate(action.x2, action.y2), action.is_valid)
             } else {
-                return new Models.Action(action.TypeAction.name, action.is_valid, new Models.Coordinate(action.x1, action.y1))
+                return new Models.Action(action_type, action.is_valid, new Models.Coordinate(action.x1, action.y1), action.is_valid)
             }
         })
 
