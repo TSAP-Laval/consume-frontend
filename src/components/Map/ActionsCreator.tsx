3074e9a42e9d302b81773947c19bddd83ad403f0
GabrielMethot <goub.methot@gmail.com>
2017-03-21 01:04:01 -0400

    Merge des maps dans dev (#29)
    * map scalable
    
    * Début de refactoring
    
    * Mini fixs avant dodo
    
    * merge
    
    * Ajout des props dans la création du component
    
    * fix
    
    * Refactoring des actions types
    
    * Fix bug rendering
    
    * Revert "Fix bug rendering"
    
    This reverts commit 61a707032d2dcf808f2a49b190e8d9bf53b79f96.
    
    * Revert "Refactoring des actions types"
    
    This reverts commit 35079f1d7f4aef06622e198d21b967e68dc345e4.
    
    * Fix display des cercles
    
    * Fix du clusterfuck
    
    * Fix majuscules dans paths

--- src/components/Map/ActionsCreator.tsx
+++ src/components/Map/ActionsCreator.tsx
@@ -10,16 +10,17 @@ export function getActions(match_id: number, player_id: number) {
     const fetch_actions = new Actions.FetchActions()
     dispatcher.dispatch(fetch_actions)
 
-    var url: string
-    url = serverUrl + "/stats/match/" + match_id + "/player/" + player_id
+    let url = serverUrl + "/stats/match/" + match_id + "/player/" + player_id
 
     axios.get(url).then((response) => {
         var data = response.data.actions
         var actions = data.map((action: any) => {
+            let action_type = new Models.ActionType(action.TypeAction.name, action.TypeAction.description)
+
             if(action.x2 != -1 && action.y2 != -1) {
-                return new Models.Action(action.TypeAction.name, action.is_valid, new Models.Coordinate(action.x1, action.y1), new Models.Coordinate(action.x2, action.y2))
+                return new Models.Action(action_type, new Models.Coordinate(action.x1, action.y1), new Models.Coordinate(action.x2, action.y2), action.is_valid)
             } else {
-                return new Models.Action(action.TypeAction.name, action.is_valid, new Models.Coordinate(action.x1, action.y1))
+                return new Models.Action(action_type, new Models.Coordinate(action.x1, action.y1), null, action.is_valid)
             }
         })
 
@@ -29,3 +30,27 @@ export function getActions(match_id: number, player_id: number) {
         CreateErrorAction(error);
     })
 }
+
+export function getMapParameters(team_id: number) {
+     let url = serverUrl + "/teams/" + team_id + "/map";
+     axios.get(url).then((response) => {
+         var data = response.data;
+         if(data.id == 0) {
+             data.width = 4;
+             data.height = 3;
+         }
+         var parameters = new Models.Size(data.width, data.height);
+         const receive = new Actions.ReceiveMapParameters(parameters);
+         dispatcher.dispatch(receive);
+     }).catch((error) => {
+         CreateErrorAction(error);
+     })
+ }
+ 
+ export function setMapParameters(team_id: number, params: Models.Size) {
+     let url = serverUrl + "/teams/" + team_id + "/map";
+     axios.post(url, {height:params.height, width: params.width}).then((response) => {
+     }).catch((error) => {
+         CreateErrorAction(error);
+     })
+ }
--- src/components/Map/Models.tsx
+++ src/components/Map/Models.tsx
@@ -1,21 +1,36 @@
-import IAction from "../IAction"
+export class IPlayerActions {
+    match_id: string
+    date: string
+    actions: Action[]
 
-export class Action implements IAction {
-    type: string
+    constructor(match_id: string, date: string, actions: Action[]) {
+        this.match_id = match_id
+        this.date = date
+        this.actions = actions
+    }
+}
 
+export class Action {
+    type: ActionType
     start: Coordinate
     end: Coordinate
     is_positive: boolean
 
-    constructor(type: string, is_positive: boolean, start: Coordinate, end?: Coordinate) {
+    constructor(type: ActionType, start: Coordinate, end: Coordinate, is_positive: boolean) {
         this.type = type
-        this.is_positive = is_positive
         this.start = start
         this.end = end
+        this.is_positive = is_positive
     }
+}
 
-    getCoordinates() {
-        return [this.start.x, this.start.y, this.end.x, this.end.y]
+export class ActionType {
+    name: string
+    description: string
+
+    constructor(name: string, description: string) {
+        this.name = name
+        this.description = description
     }
 }
 
@@ -26,5 +41,37 @@ export class Coordinate {
     constructor(x: number, y: number) {
         this.x = x
         this.y = y
+    }
+}
+
+export class Size {
+    width: number
+    height: number
+
+    constructor(width: number, height: number) {
+        this.width = width
+        this.height = height
+    }
+}
+
+export class Zone {
+    coordinate: Coordinate
+    percentage: number
+    rating: number
+
+    constructor(coordinate: Coordinate, percentage: number, rating: number) {
+        this.coordinate = coordinate
+        this.percentage = percentage
+        this.rating = rating
+    }
+}
+
+export class ZoneData {
+    coordinate: Coordinate
+    is_positive: boolean
+
+    constructor(coordinate: Coordinate, is_positive: boolean) {
+        this.coordinate = coordinate
+        this.is_positive = is_positive
     }
 }
