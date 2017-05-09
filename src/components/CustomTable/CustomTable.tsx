8b586929cae2ca58d05c910f39b1f5353ff10a4c
Gabriel Méthot <Gabriel Mthot>
2017-05-08 20:58:35 -0400

    Ajout custom table

---
+++ src/components/CustomTable/CustomTable.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import styled from 'styled-components';
+import { Table, TableBody, TableRow, TableHeader, TableHeaderColumn } from 'material-ui/Table';
+
+export interface IDataProps {
+    columns: Array<Array<String>>
+}
+
+export interface IDataStates {
+    title: string
+}
+
+const StyledTable = styled(Table)`
+    text-align: center;
+`;
+
+export default class CustomTable extends React.Component<IDataProps, IDataStates> {
+    render() {
+        let headers = this.props.columns.map((h, i) => {
+            return <TableHeaderColumn key={ i.toString() }><span>{ h[0] }</span><br/>{h[1]}</TableHeaderColumn>
+        });
+
+        return (
+            <StyledTable className="custom-table">
+                <TableHeader displaySelectAll={false} adjustForCheckbox={false}><TableRow>{ headers }</TableRow></TableHeader>
+                <TableBody>
+                    { this.props.children }
+                </TableBody>
+            </StyledTable>
+        );
+    }
+}
