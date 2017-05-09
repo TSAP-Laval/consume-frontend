134183a69c5a06bb6a687b2565e2a053d919c0c7
Gabriel Méthot <Gabriel Mthot>
2017-05-08 20:41:19 -0400

    Modification des modèles

---
+++ src/Models/ComponentModels.tsx
@@ -0,0 +1,64 @@
+import {Coordinate} from "./DatabaseModels"
+
+export interface IFilterable {
+    toFilterNode(): FilterNode;
+}
+
+export class Filter {
+    name: string;
+    nodes: Array<FilterNode>;
+    colored: boolean;
+
+    constructor(name: string, nodes: Array<FilterNode> = new Array<FilterNode>(), colored: boolean = false) {
+        this.name = name;
+        this.nodes = nodes;
+        this.colored = colored;
+    }
+}
+
+export class FilterNode {
+    label: string;
+    value: string;
+    used: boolean;
+    color: RGBColor;
+
+    constructor(label: string, value: string, used: boolean = true, color: RGBColor = null) {
+        this.label = label;
+        this.value = value;
+        this.used = used;
+    }
+}
+
+export class Zone {
+    coordinate: Coordinate;
+    percentage: number;
+    rating: number;
+
+    constructor(coordinate: Coordinate, percentage: number, rating: number) {
+        this.coordinate = coordinate;
+        this.percentage = percentage;
+        this.rating = rating;
+    }
+}
+
+export class ZoneData {
+    coordinate: Coordinate;
+    is_positive: boolean;
+
+    constructor(coordinate: Coordinate, is_positive: boolean) {
+        this.coordinate = coordinate;
+        this.is_positive = is_positive;
+    }
+}
+
+export class RGBColor {
+    r: number;
+    g: number;
+    b: number;
+
+    constructor(r: number, g: number, b: number) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+}
---
+++ src/Models/DatabaseModels.tsx
@@ -0,0 +1,135 @@
+import {IFilterable, FilterNode} from "./ComponentModels"
+
+export class Action {
+    id: number;
+    player_id: number;
+    type: ActionType;
+    start: Coordinate;
+    end: Coordinate;
+
+    constructor(id: number, player_id: number, type: ActionType, start: Coordinate, end: Coordinate = null) {
+        this.id = id;
+        this.player_id = player_id;
+        this.type = type;
+        this.start = start;
+        this.end = end;
+    }
+}
+
+export class ActionType implements IFilterable{
+    id: number;
+    name: string;
+    description: string;
+    is_positive: boolean;
+
+    constructor(id: number, name: string, description: string, is_positive: boolean) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.is_positive = is_positive;
+    }
+
+    toFilterNode() {
+        return new FilterNode(this.description, this.id.toString());
+    }
+}
+
+export class Player {
+    id: number;
+    first_name: string;
+    last_name: string;
+
+    constructor(id: number, first_name: string, last_name: string) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+}
+
+export class PlayerDataByMatch {
+    id: number;
+    player_id: number;
+    match_id: number;
+    number: number;
+    position: Position;
+
+    constructor(id: number, player_id: number, match_id: number, number: number, position: Position) {
+        this.id = id;
+        this.player_id = player_id;
+        this.match_id = match_id;
+        this.number = number;
+        this.position = position;
+    }
+}
+
+export class Position {
+    id: number;
+    description: string;
+
+    constructor(id: number, description: string) {
+        this.id = id;
+        this.description = description;
+    }
+}
+
+export class Match {
+    id: number;
+    home_team_id: number;
+    away_team_id: number;
+    date: Date;
+    location: string;
+
+    constructor(id: number, home_team_id: number, away_team_id: number, date: Date, location: string) {
+        this.id = id;
+        this.home_team_id = home_team_id;
+        this.away_team_id = away_team_id;
+        this.date = date;
+        this.location = location;
+    }
+}
+
+export class Season {
+    id: number;
+    start_year: number;
+    end_year: number;
+
+    constructor(id: number, start_year: number, end_year: number) {
+        this.id = id;
+        this.start_year = start_year;
+        this.end_year = end_year;
+    }
+}
+
+export class Metric {
+    id: number;
+    name: string;
+    equation: string;
+
+    constructor(id: number, name: string, equation: string) {
+        this.id = id;
+        this.name = name;
+        this.equation = equation;
+    }
+}
+
+export class MetricData {
+    metric: Metric;
+    value: number;
+    deviation: number;
+
+    constructor(metric: Metric, value: number = 0, deviation:number = 0) {
+        this.metric = metric;
+        this.value = value;
+        this.deviation = deviation;
+    }
+}
+
+export class Coordinate {
+    x: number;
+    y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+}
