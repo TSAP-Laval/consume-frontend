9e50900ae08b796f8d1f3a91ef568dfcb9ed1743
William Dussault <dalloriam@gmail.com>
2017-05-21 16:32:59 -0400

    Fix display metrics

--- src/components/TeamSettingsView/Actions/UpdateMetric.ts
+++ src/components/TeamSettingsView/Actions/UpdateMetric.ts
@@ -2,7 +2,7 @@ import { IAction } from "../../../models/ActionCreation";
 import { CreateErrorAction } from "../../Error/ErrorAction";
 import { CreateFetchMetricsAction } from './FetchMetrics';
 import { Metric } from "../MetricModel";
-import axios, {AxiosResponse} from 'axios';
+import axios from 'axios';
 import Dispatcher from "../../Dispatcher";
 import * as Config from 'Config';
 
@@ -18,19 +18,23 @@ export class UpdateMetricAction implements IAction {
     }
 }
 
-export function CreateUpdateMetricAction(metric: Metric, teamID: number) {
+export function CreateUpdateMetricAction(metric: Metric, teamID: number, token: string) {
     Dispatcher.dispatch(new UpdateMetricAction(metric));
 
-    let url: string = Config.serverUrl + "/teams/" + teamID + "/metrics/" + metric.id.toString();
+    let instance = axios.create({
+        headers: {"X-Auth-Token": token}
+    });
 
-    axios.put(url, {
+    let url: string = Config.serverUrl + "teams/" + teamID + "/metrics/" + metric.id.toString();
+
+    instance.put(url, {
         name: metric.name,
         description: metric.description,
         formula: metric.formula
     })
     .then(
-        (resp: AxiosResponse) => {
-            CreateFetchMetricsAction(teamID);
+        () => {
+            CreateFetchMetricsAction(teamID, token);
         },
         (err) => {
             CreateErrorAction(err.toString());
