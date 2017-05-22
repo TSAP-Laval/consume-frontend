4294f73997684fe81480140fefb2bbc9716d51d1
kaizkiller <kaizkiller@bitbucket.org>
2017-05-21 21:50:27 -0400

    Partial commit

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -10,6 +10,8 @@ export interface ILoginProps {
 export interface ILoginState {
     email?: string,
     password?: string,
+    emailError?:string,
+    passwordError?:string,
 }
 
 export default class Login extends React.Component<ILoginProps, ILoginState> {
@@ -19,9 +21,12 @@ export default class Login extends React.Component<ILoginProps, ILoginState> {
         this.onLogin = this.onLogin.bind(this);
         this.handleEmailChange = this.handleEmailChange.bind(this);
         this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.validateEmail = this.validateEmail.bind(this);
           this.state = {
             email: '',
-            password:''
+            password:'',
+            emailError: '',
+            passwordError: ''
         }
     }
 
@@ -31,6 +36,18 @@ export default class Login extends React.Component<ILoginProps, ILoginState> {
 
     handleEmailChange(e: any) {
         this.setState({ email:  e.target.value})
+    }
+
+    validateEmail(e: any){
+        let regex  = /^[a-z][a-zA-Z0-9_]*(\.[a-zA-Z][a-zA-Z0-9_]*)?@[a-z][a-zA-Z-0-9]*\.[a-z]+(\.[a-z]+)?$/;
+        console.log("Avant le if de la validation.");
+        if (e.target.value.match(regex)) {
+            this.setState({emailError: "Veuillez entrer une email valide."})
+            console.log("Dans la validation");
+        } else{
+            this.setState({emailError: ''})
+            console.log("Dans le else de la validation");
+        }
     }
 
     handlePasswordChange(e: any) {
@@ -50,7 +67,9 @@ export default class Login extends React.Component<ILoginProps, ILoginState> {
                     fullWidth={true}
                     value ={this.state.email}
                     name='email'
+                    errorText = {this.state.emailError}
                 onChange={this.handleEmailChange}
+                onBlur = {this.validateEmail}
                 />
                 <br />
                 <TextField
@@ -60,6 +79,7 @@ export default class Login extends React.Component<ILoginProps, ILoginState> {
                     fullWidth={true}
                     type="password"
                     name='password'
+                    errorText = {this.state.passwordError}
                 onChange={this.handlePasswordChange}
                 />
                 <div style={{ float: 'right' }}>
