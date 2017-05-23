0f0378499b393b658f50edb4d676052c229cb701
kaizkiller <kaizkiller@bitbucket.org>
2017-05-23 08:48:13 -0400

    Added validations in create user's form.

--- karma.conf.js
+++ karma.conf.js
@@ -4,9 +4,9 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const sassLoaders = [
-  'css-loader',
-  'postcss-loader',
-  'sass-loader?includePaths[]='+ path.resolve(__dirname, 'node_modules')
+    'css-loader',
+    'postcss-loader',
+    'sass-loader?includePaths[]=' + path.resolve(__dirname, 'node_modules')
 ];
 
 module.exports = function(config) {
@@ -21,48 +21,7 @@ module.exports = function(config) {
             './test/test_index.js': ['webpack', 'sourcemap']
         },
         webpack: {
-            devtool: 'inline-source-map',
-            plugins: [
-                new ExtractTextPlugin('bundle.css')
-            ],
-            postcss: [
-                autoprefixer({
-                    browsers: ['last 2 versions']
-                })
-            ],
-            module: {
-                loaders: [{
-                        test: /\.(ts|tsx)$/,
-                        exclude: /node_modules/,
-                        loader: 'ts-loader'
-                    },
-                    //Configuration required by enzyme
-                    {
-                        test: /\.json$/,
-                        include: /node_modules/,
-                        loader: 'json-loader'
-                    },
-                    {
-                        test: /\.scss?$/,
-                        loader: ExtractTextPlugin.extract('style-loader', sassLoaders.join('!'))
-                    }
-                ]
-            },
-            resolve: {
-                //Added .json extension required by cheerio (enzyme dependency)
-                extensions: ['', '.js', '.ts', '.tsx', '.json', '.scss']
-            },
-            //Configuration required by enzyme
-            externals: {
-                'react/addons': true,
-                'react/lib/ExecutionEnvironment': true,
-                'react/lib/ReactContext': 'window',
-                'Config': JSON.stringify(debug ? {
-                    serverUrl: "http://localhost:8080/api"
-                } : {
-                    serverUrl: "/api"
-                })
-                    }
+
         },
         webpackMiddleware: {
             // webpack-dev-middleware configuration
